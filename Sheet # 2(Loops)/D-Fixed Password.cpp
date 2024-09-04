daf2ce1f7e9002bc852b8cb4aebe6bb5f7cf07de
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-09-04 18:53:05 +0600

    Condition

---
+++ Sheet # 2(Loops)/D-Fixed Password.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int pass=1999,input;
+ while(true)
+ {
+    cin>>input;
+    if(input==pass)
+    {
+        cout<<"Correct"<<endl;
+        break;
+    }
+    else 
+    {
+        cout<<"Wrong"<<endl;
+ 
+    }
+ 
+ }
+ 
+ 
+}
