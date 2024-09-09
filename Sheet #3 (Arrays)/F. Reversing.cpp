c63dabfae0431214073bf4f2b32b25960e6023c5
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-09-09 20:56:34 +0600

    Rev of a array

---
+++ Sheet #3 (Arrays)/F. Reversing.cpp
@@ -0,0 +1,27 @@
+
+#include<bits/stdc++.h>
+ 
+using namespace std;
+int main()
+{
+   
+   int test_case,test_input,x,min_value,min_position;
+   cin>>test_case;
+   int arry_input[test_case];
+   for(int i=0;i<test_case;i++)
+   {
+    cin>>arry_input[i];
+   }
+   for(int a=test_case-1;a>=0;a--)
+   {
+     
+    cout<<arry_input[a]<<" ";
+   }
+   
+   
+ return 0;
+}
+ 
+ 
+ 
+ 
