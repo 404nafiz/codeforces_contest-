2173a0e42b1c8b40390e16d3ee8f7cd9ea5fb206
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-09-15 19:12:39 +0600

    Create A.-Timon and Pumbaa.cpp

---
+++ Contest #2/A.-Timon and Pumbaa.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()     
+{
+   int a,b,value;
+   cin>>a>>b;
+   value=a-b;
+   if(value>=0) cout<<value;
+   else cout<<"";
+   
+    return 0;
+}
