fb23afc67d0bd88446e8eee29112de687af351bb
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2024-11-01 20:16:12 +0600

    Create G- Factorial.cpp

---
+++ Sheet # 2(Loops)/G- Factorial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a,b,factorial=1;
+cin>>a;
+for(int i=1;i<=a;i++)
+{
+ cin>>b;
+ for(int i=1;i<=b;i++)
+ {
+   factorial=factorial*i;
+
+ }
+ cout<<factorial<<endl;
+ factorial=1;
+}
+
+
+}
